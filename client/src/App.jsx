d9b421016fb7f36862fe0548a8d838062d0d53d5
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-04-27 20:31:24 +0530

    Made header and button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <div className="head-text">
-    <h1>HEEEE</h1>
-
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+      
+    </main>
   )
 }
 
