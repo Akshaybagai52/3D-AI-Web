7e01a15a765091babdf126f5e8d4598b3613aa1c
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-04-29 23:17:39 +0530

    updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./CustomButton";
+import AiPicker from "./AiPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton }
+export { CustomButton, AiPicker, ColorPicker, FilePicker, Tab  }
