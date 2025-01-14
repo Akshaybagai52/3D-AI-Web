29975ccce292e18741d72ea3407520efa44f8ca3
Akshaybagai52 <akshaybagai52@gmail.com>
2025-01-14 17:07:35 +0530

    fix: fixed Environment tag issue

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -12,7 +12,7 @@ const CanvasModel = () => {
      gl={{ preserveDrawingBuffer: true }}
      className="w-full max-w-full h-full transition-all ease-in" >
       <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+      {/* <Environment preset="city" /> */}
 
       <CamerRig>
         <Backdrop />
